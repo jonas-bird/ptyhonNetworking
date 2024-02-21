34aec53e3c9c8baadddc58bfaa3307ba686dded0
Jonas Bird <jonas.bird.m@gmail.com>
2024-02-21 18:14:37 -0500

    feat: add logic to accept connections

--- src/httpServer.py
+++ src/httpServer.py
@@ -2,40 +2,53 @@
 """
 Simple HTTP Server code from Computer Networking a Top Down Approach
 """
-#import socket module
+# import socket module
 from socket import *
-import sys # In order to terminate the program
+import sys   # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-#Prepare a sever socket
-#Fill in start
-#Fill in end
-#
-while True:
-    #Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr =   #Fill in start  #Fill in end
-    try:
-        message =    #Fill in start   #Fill in end
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = #Fill in start   #Fill in end
+# Prepare a sever socket
+# Fill in start
+HOST = "127.0.0.1"
+serverPort = 8383
+serverSocket.bind((HOST, serverPort))
+serverSocket.listen(1)
+# Fill in end
 
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        #Send the content of the requested file to the client
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()   # Fill in start  #Fill in end
+    try:
+        message = connectionSocket.recv(1024).decode()   # Fill in start   #Fill in end
+        filename = message.split()[1]
+        # f = open(filename[1:])
+        outputdata = "HTTP/1.0 200 OK\n\n"
+        outputdata += "Hello World\n"
+
+        # Send one HTTP header line into socket
+        # Fill in start
+        # Fill in end
+        # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     except IOError:
-        #Send response message for file not found
-        #Fill in start
-        #Fill in end
-        #Close client socket
-        #Fill in start
-        #Fill in end
+        # Send response message for file not found
+        # Fill in start
+
+        print('Something went wrong')  # Should have logging if this was a real program
+        outputdata = "HTTP/1.0 404 Not Found"
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        # Fill in end
+        # Close client socket
+        # Fill in start
+
+        connectionSocket.close()
+        # Fill in end
 serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+sys.exit()   # Terminate the program after sending the corresponding data
